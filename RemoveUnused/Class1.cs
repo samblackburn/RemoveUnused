9e3a72cc98e85d73400b7d374628abb8e2fa1905
Sam Blackburn <sam.blackburn@red-gate.com>
2020-10-30 17:50:54 +0000

    Example from https://joshvarty.com/2014/07/06/learn-roslyn-now-part-2-analyzing-syntax-trees-with-linq/

---
+++ RemoveUnused/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RemoveUnused
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public async Task Foo()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+        }
+    }");
+
+            var syntaxRoot = tree.GetRoot();
+            var MyClass = syntaxRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+            var MyMethod = syntaxRoot.DescendantNodes().OfType<MethodDeclarationSyntax>().First();
+
+            Console.WriteLine(MyClass.Identifier.ToString());
+            Console.WriteLine(MyMethod.Identifier.ToString());
+        }
+    }
+}
