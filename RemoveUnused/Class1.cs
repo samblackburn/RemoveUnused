54642fe67d55069b695c1e4052c0a4d8ed28b3f6
Sam Blackburn <sam.blackburn@red-gate.com>
2020-11-06 18:14:54 +0000

    Make it work for separate referenced/referencing projects

--- RemoveUnused/Class1.cs
+++ RemoveUnused/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using NUnit.Framework;
 using Microsoft.CodeAnalysis.CSharp;
@@ -42,6 +44,64 @@ namespace RemoveUnused
             foreach (var reference in references.Select(x => x.Result))
             {
                 Console.WriteLine($"{reference.Referenced.First()} --> {reference.Referencing.First()}");
+            }
+        }
+
+        [Test]
+        public async Task TwoProjects()
+        {
+            const string referencing = @"public class Consumer{public void Consumer(){Consumed.Consumed();}}";
+            const string referenced = @"public class Consumed{public static void Consumed(){}}";
+
+            var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var ws = new AdhocWorkspace();
+            var project1 = ws.AddProject("Referenced", "C#").AddMetadataReference(mscorlib);
+            var project2 = ws.AddProject("Referencing", "C#").AddMetadataReference(mscorlib);
+            ws.TryApplyChanges(project1.Solution);
+            ws.TryApplyChanges(project2.Solution);
+            var documents = new[]
+            {
+                ws.AddDocument(project1.Id, "Referenced", SourceText.From(referenced)),
+                ws.AddDocument(project1.Id, "Referencing", SourceText.From(referencing))
+            };
+
+            var models = documents.Select(d => d.GetSemanticModelAsync().Result);
+            //Get the syntax nodes for all method invocations
+            var methodInvocations = documents.SelectMany(d => d.GetSyntaxRootAsync().Result.DescendantNodes().OfType<InvocationExpressionSyntax>());
+            var methodSymbols = models.SelectMany(m => methodInvocations.SelectMany(i => TryGetSymbol(i, m)));
+            //Finds all references to each method
+            var references = methodSymbols.Select(m => new
+            {
+                Referenced = m.Locations,
+                Referencing = documents
+                    .SelectMany(d => SymbolFinder.FindReferencesAsync(m, d.Project.Solution).Result)
+                    .SelectMany(x => x.Locations)
+                    .Select(x => x.Location)
+            });
+
+            foreach (var reference in references)
+            {
+                Console.WriteLine($"{reference.Referenced.First()} --> {reference.Referencing.First()}");
+            }
+        }
+
+        private static IEnumerable<ISymbol> TryGetSymbol(
+            ExpressionSyntax invocationExpressionSyntax,
+            SemanticModel semanticModel)
+        {
+            ISymbol result;
+            try
+            {
+                result = semanticModel.GetSymbolInfo(invocationExpressionSyntax).Symbol;
+            }
+            catch
+            {
+                yield break;
+            }
+
+            if (result != null)
+            {
+                yield return result;
             }
         }
     }
