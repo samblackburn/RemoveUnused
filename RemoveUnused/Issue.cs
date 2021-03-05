e1b1d9c84447c6158995511b507d23959db1c930
Sam Blackburn <sam.blackburn@red-gate.com>
2021-03-05 14:58:44 +0000

    XML parsing

---
+++ RemoveUnused/Issue.cs
@@ -0,0 +1,12 @@
+﻿namespace RemoveUnused
+{
+    internal class Issue
+    {
+        public string TypeId { get; set; }
+        public string File { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Line { get; set; }
+        public string Message { get; set; }
+    }
+}
